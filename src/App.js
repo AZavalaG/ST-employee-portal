91c80525d03e1cd5140784141e7c2ebed1d581dc
CarlosMorenoSTGroup <cmoreno@stgroup.mx>
2023-06-23 13:25:38 -0600

    EP-E01H09:DEV: Clean App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import PersonCard from "./components/PersonCard/PersonCard";
 
 const App = () => {
   return (
     <div>
       <h1>Employee Portal!</h1>
-      <PersonCard />
     </div>
   );
 };

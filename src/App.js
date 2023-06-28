1399b89c17bef5ecb174026006801618501ca979
Guillermo Garcia pascual <guillermogarciapascual@Guillermos-MacBook-Air.local>
2023-06-28 08:57:26 -0600

    Fix Issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { ScreenTitleBarTeams } from './Components/index';
-
 
 const App = () => {
-  return (
-    <div>
-      <h1>Employee Portal!</h1>
-      <ScreenTitleBarTeams/>
-    </div>
-  );
+  return ( 
+    <h1>Employee Portal!</h1>
+   );
 }
-
+ 
 export default App;
-

7462f2df1ddba8a820e36ba45c44f8ee8e31648c
CarlosMorenoSTGroup <cmoreno@stgroup.mx>
2023-07-04 13:47:19 -0600

    EP:E04H01:DEV.Class Change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Employee Portal!</h1>
-    </div>
-  );
-};
-
+  return ( 
+    <h1>Employee Portal!</h1>
+   );
+}
+ 
 export default App;

1303436e1b7733fbed8f3fce98fa282fe99159ef
Gbaires-ST <137438599+Gbaires-ST@users.noreply.github.com>
2023-07-06 10:41:41 -0600

    Modifications for the sidebar and menubar using css

--- src/Components/ProfileUser/ProfileUser.jsx
+++ src/Components/ProfileUser/ProfileUser.jsx
@@ -1,18 +1,22 @@
+import styles from './profileUser.module.css';
 import React, { Component } from 'react';
 import Navbar from '../Navbar';
 import { SearchBar } from '../SearchBar';
-const role = localStorage.getItem("role");
+/*const role = localStorage.getItem("role");*/
+
 
 class ProfileUsr extends Component {
     render() {
         return (
-            <div>
-                <div></div>
-                <Navbar />
+            <div >  
+                <div className= {styles.menubar}>
+                <Navbar/>         
+                </div> 
+                <div className={styles.sidebar}>
                 <SearchBar />
+                </div>
             </div>
         );
     }
 }
- 
-export default ProfileUsr;
+ export default ProfileUsr;

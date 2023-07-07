3a800b7b2fe8859aa554b133a95cb863cd46e48f
Victor <vbertoncini@stgroup.mx>
2023-07-07 12:51:25 -0300

    Add profile picture

--- src/Components/ProfileUser/ProfileUser.jsx
+++ src/Components/ProfileUser/ProfileUser.jsx
@@ -19,6 +19,15 @@ class ProfileUsr extends Component {
                 <SearchBar />
                 </div>
                 <div className={styles.profile}>
+                    <div className={styles.user_avatar}>
+                        <figure className={styles.profilePicture}>
+                            <img className={`${styles['rounded-photo']}`} src='https://images.pexels.com/photos/1381112/pexels-photo-1381112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+                        </figure> 
+                        <h1 className={styles.title}>{DataUser?.name || ''}</h1>
+                    </div>
+                    <button className={styles.return} onClick={console.log('test')}>
+                        <img src="https://github.com/AZavalaG/ST-employee-portal/blob/EP-E02H01/src/Assets/back-cta.png?raw=true" width="20" height="20"/>
+                    </button>
                 <UserInfo />
                 </div>
             </div>

1b70a1df5d9d33d273ab48903eeb1c4076e5f1f1
CarlosMorenoSTGroup <cmoreno@stgroup.mx>
2023-07-07 15:25:40 -0600

    EP:E04H03 manager view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import TimeEntry from "./Components/TimeEntry";
 
 const App = () => {
-  return ( 
-    <h1>Employee Portal!
+  return (
+    <h1>
+      Employee Portal!
+      <TimeEntry />
     </h1>
-    
-   );
-}
- 
+  );
+};
+
 export default App;
---
+++ src/Components/TimeEntry/dummyData.jsx
@@ -0,0 +1,212 @@
+export const data = [
+  {
+    proyectName: "Service Now",
+    visible:true,
+    calender:{
+        0:"03/07",
+        1:"04/07",
+        2:"05/07",
+        3:"06/07",
+        4:"07/07",
+    },
+    phases: [
+      {
+        phaseName: "initial Configuration",
+        comment: "test name",
+        calender: {
+          0: 3,
+          1: 3,
+          2: 3,
+          3: 3,
+          4: 3,
+        },
+        tasks: [
+          {
+            taskName: "Bussines Rules",
+            calender: {
+              0: 3,
+              1: 0,
+              2: 0,
+              3: 0,
+              4: 3,
+            },
+            comment: "",
+          },
+          {
+            taskName: "Wizard Configuration",
+            calender: {
+              0: 0,
+              1: 3,
+              2: 0,
+              3: 3,
+              4: 0,
+            },
+            comment: "",
+          },
+          {
+            taskName: "Portal COnfiguration",
+            calender: {
+              0: 0,
+              1: 0,
+              2: 3,
+              3: 0,
+              4: 0,
+            },
+            comment: "data dummy",
+          },
+        ],
+      },
+      {
+        phaseName: "initial Configuration Gruop 2",
+        comment: "",
+        calender: {
+          0: 3,
+          1: 3,
+          2: 3,
+          3: 3,
+          4: 3,
+        },
+        tasks: [
+          {
+            taskName: "User Config",
+            calender: {
+              0: 3,
+              1: 0,
+              2: 0,
+              3: 0,
+              4: 3,
+            },
+            comment: "",
+          },
+          {
+            taskName: "DAshboard settings",
+            calender: {
+              0: 0,
+              1: 3,
+              2: 0,
+              3: 3,
+              4: 0,
+            },
+            comment: "",
+          },
+          {
+            taskName: "Impport Data",
+            calender: {
+              0: 0,
+              1: 0,
+              2: 3,
+              3: 0,
+              4: 0,
+            },
+            comment: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    proyectName: "Sales Forc",
+    visible:true,
+    calender:{
+        0:"03/07",
+        1:"04/07",
+        2:"05/07",
+        3:"06/07",
+        4:"07/07",
+    },
+    phases: [
+      {
+        phaseName: "initial Configuration",
+        comment: "test name",
+        calender: {
+          0: 3,
+          1: 3,
+          2: 3,
+          3: 3,
+          4: 3,
+        },
+        tasks: [
+          {
+            taskName: "Bussines Rules",
+            calender: {
+              0: 3,
+              1: 0,
+              2: 0,
+              3: 0,
+              4: 3,
+            },
+            comment: "",
+          },
+          {
+            taskName: "Wizard Configuration",
+            calender: {
+              0: 0,
+              1: 3,
+              2: 0,
+              3: 3,
+              4: 0,
+            },
+            comment: "",
+          },
+          {
+            taskName: "Portal COnfiguration",
+            calender: {
+              0: 0,
+              1: 0,
+              2: 3,
+              3: 0,
+              4: 0,
+            },
+            comment: "data dummy",
+          },
+        ],
+      },
+      {
+        phaseName: "initial Configuration Gruop 2",
+        comment: "",
+        calender: {
+          0: 3,
+          1: 3,
+          2: 3,
+          3: 3,
+          4: 3,
+        },
+        tasks: [
+          {
+            taskName: "User Config",
+            calender: {
+              0: 3,
+              1: 0,
+              2: 0,
+              3: 0,
+              4: 3,
+            },
+            comment: "",
+          },
+          {
+            taskName: "DAshboard settings",
+            calender: {
+              0: 0,
+              1: 3,
+              2: 0,
+              3: 3,
+              4: 0,
+            },
+            comment: "",
+          },
+          {
+            taskName: "Impport Data",
+            calender: {
+              0: 0,
+              1: 0,
+              2: 3,
+              3: 0,
+              4: 0,
+            },
+            comment: "",
+          },
+        ],
+      },
+    ],
+  },
+];

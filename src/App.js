2a177b4fbef6d1564195e907bc64e8d9f1cd7670
CarlosMorenoSTGroup <cmoreno@stgroup.mx>
2023-07-07 15:13:42 -0600

    EP-E04H03-DEV: timeentry

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import TimeManagement from './Components/TimeManagement';
 
 const App = () => {
   return ( 
     <h1>Employee Portal!
-      <TimeManagement />
     </h1>
     
    );
